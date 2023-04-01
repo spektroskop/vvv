5bd6b7b1a697c4021a399989ddd1e568c32950f9
Alexander Stensrud <alex@yuki.no>
2023-04-01 08:32:26 +0200

    Map models and cmds

--- client/src/Lib/Return.elm
+++ client/src/Lib/Return.elm
@@ -1,15 +1,36 @@
-module Lib.Return exposing (Return, andThen)
+module Lib.Return exposing
+    ( Return
+    , andThen
+    , map
+    , mapCmd
+    , mapModel
+    )
 
 
 type alias Return msg model =
     ( model, Cmd msg )
 
 
+mapModel : (model -> a) -> Return msg model -> Return msg a
+mapModel m ( model, command ) =
+    ( m model, command )
+
+
+mapCmd : (msg -> a) -> Return msg model -> Return a model
+mapCmd m ( model, cmd ) =
+    ( model, Cmd.map m cmd )
+
+
+map : (model1 -> model2) -> (msg1 -> msg2) -> Return msg1 model1 -> Return msg2 model2
+map mm mc =
+    mapModel mm >> mapCmd mc
+
+
 andThen : (model -> Return msg a) -> Return msg model -> Return msg a
-andThen f ( model, cmd ) =
+andThen m ( model, cmd ) =
     let
         ( newModel, newCmd ) =
-            f model
+            m model
     in
     ( newModel
     , Cmd.batch [ cmd, newCmd ]
