0ec2b492827793f6255dc233e3886b32bc9463f4
Alexander Stensrud <alex@yuki.no>
2023-04-05 05:38:14 +0200

    Use hacky gleam bundler for lustre

--- frontends/lustre/assets/app.js
+++ frontends/lustre/assets/app.js
@@ -1,5 +1,3 @@
-// TODO: bundler support and something like this?
-// import { main } from "./src/vvv.gleam"
-import { main } from "../build/dev/javascript/vvv/vvv.mjs"
+import { main } from "../src/vvv.gleam"
 
 main()
