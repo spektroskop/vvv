1225d5e1dba6cfe944c81e2ca43cf664c230b4ff
Alexander Stensrud <alex@yuki.no>
2023-04-09 08:46:25 +0200

    Cleanup

--- frontends/custom/assets/app.js
+++ frontends/custom/assets/app.js
@@ -2,12 +2,11 @@ import {main} from "../src/vvv.gleam"
 
 main()
 
-function wsURL() {
+let wsURL = () => {
   let {protocol, host} = window.location
   return protocol === "https:" ? `wss://${host}/ws` : `ws://${host}/ws`
 }
 
-let {protocol, host} = window.location
 let ws = new WebSocket(wsURL())
 
 ws.onmessage = (event) => {
