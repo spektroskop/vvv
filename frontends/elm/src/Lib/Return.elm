9232dd95db6d2da15bad5af6f58ef94db60b337b
Alexander Stensrud <alex@yuki.no>
2023-04-28 02:22:19 +0200

    Support Maybe models

--- frontends/elm/src/Lib/Return.elm
+++ frontends/elm/src/Lib/Return.elm
@@ -4,6 +4,8 @@ module Lib.Return exposing
     , map
     , mapCmd
     , mapModel
+    , maybe
+    , withDefault
     )
 
 
@@ -21,9 +23,27 @@ mapCmd m ( model, cmd ) =
     ( model, Cmd.map m cmd )
 
 
-map : (model1 -> model2) -> (msg1 -> msg2) -> Return msg1 model1 -> Return msg2 model2
+map :
+    (model1 -> model2)
+    -> (msg1 -> msg2)
+    -> Return msg1 model1
+    -> Return msg2 model2
 map mm mc =
     mapModel mm >> mapCmd mc
+
+
+maybe :
+    (model1 -> model2)
+    -> (msg1 -> msg2)
+    -> Return msg1 (Maybe model1)
+    -> Return msg2 (Maybe model2)
+maybe =
+    Maybe.map >> map
+
+
+withDefault : a -> Return msg (Maybe a) -> Return msg a
+withDefault =
+    Maybe.withDefault >> mapModel
 
 
 andThen : (model -> Return msg a) -> Return msg model -> Return msg a
